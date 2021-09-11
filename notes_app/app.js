9ceaa9797ebf2a43b555e3c450816813f7bc36f5
Manish <Manish.Girdhar@hotmail.com>
2021-09-11 15:09:56 +1000

    First commit

---
+++ notes_app/app.js
@@ -0,0 +1,6 @@
+
+const validator = require('validator');
+const fs =require('fs');
+const myString  =require('./util.js');
+console.log(validator.isEmail("test@anz.com"));
+console.log(myString);
---
+++ notes_app/util.js
@@ -0,0 +1,3 @@
+console.log('Other utility Js file.')
+const myString = 'StringObject';
+module.exports = myString;
